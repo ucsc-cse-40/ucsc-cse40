f5a70580d31211c7963afa862f3f1070f606045a
Eriq Augustine <eriq.augustine@gmail.com>
2023-09-28 09:13:31 -0500

    Updated the autograder-py version.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     name = 'ucsc-cse40',
     url = 'https://github.com/ucsc-cse-40/ucsc-cse40',
 
-    version = '0.7.1',
+    version = '0.7.2',
     keywords = 'grading',
 
     description = "Dependencies for UCSC's CSE 40 - ML Basics: Data Analysis and Empirical Methods",
@@ -41,7 +41,7 @@ setuptools.setup(
         'numpy>=1.22.3',
         'pandas>=1.5.1',
         'scikit-learn>=1.2.0',
-        'autograder-py==0.2.1',
+        'autograder-py==0.2.2',
     ],
 
     license_files = (LICENSE_PATH, ),
